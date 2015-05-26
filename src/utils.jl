84bc52e8b94d12c45553b75ccbf572411bb0ff7d
Mladen Kolar <mkolar@gmail.com>
2015-05-26 08:20:43 -0500

    local changes

--- src/utils.jl
+++ src/utils.jl
@@ -25,28 +25,35 @@ function prepareLowRankNeighborhoodData{T<:FloatingPoint}(
   # we find a low rank approximation to A
   i1 = 1:K
   i2 = K+1:size(D, 2)
-  svdD = svdfact(D)
-  S = svdD.S
-  m = length(S)
-  k = 0
-  @inbounds while k < m
-    k += 1
-    v = S[k].^2. / n
-    if v < min_eigval
-      k -= 1
-      break
-    end
-    S[k] = v
-  end
-  resize!(S, k)
-  U1 = svdD[:Vt][1:k, i1]'
-  U2 = svdD[:Vt][1:k, i2]'
-  tmp = U1*Diagonal(S)
-  ef = eigfact(Symmetric(Diagonal(S)-tmp'*((tmp*(Diagonal(S)\tmp'))\tmp)), min_eigval, Inf)
-  A22 = LowRankEigen(U2 * ef.vectors, ef.values)
-  A12 = U1 * (Diagonal(S) * U2')
-  ef = eigfact(Symmetric(U1*(Diagonal(S)*U1')), min_eigval, Inf)
+  A = D'D / n
+  ef = eigfact(Symmetric(A[i1, i1]), min_eigval, Inf)
   A11 = LowRankEigen(ef.vectors, ef.values)
+  A12 = A[i1, i2]
+  ef = eigfact(Symmetric(A[i2, i2] - A12' * (A11 \ A12)), min_eigval, Inf)
+  A22 = LowRankEigen(ef.vectors, ef.values)
+
+#   svdD = svdfact(D)
+#   S = svdD.S
+#   m = length(S)
+#   k = 0
+#   @inbounds while k < m
+#     k += 1
+#     v = S[k].^2. / n
+#     if v < min_eigval
+#       k -= 1
+#       break
+#     end
+#     S[k] = v
+#   end
+#   resize!(S, k)
+#   U1 = svdD[:Vt][1:k, i1]'
+#   U2 = svdD[:Vt][1:k, i2]'
+#   tmp = U1*Diagonal(S)
+#   ef = eigfact(Symmetric(Diagonal(S)-tmp'*((tmp*(Diagonal(S)\tmp'))\tmp)), min_eigval, Inf)
+#   A22 = LowRankEigen(U2 * ef.vectors, ef.values)
+#   A12 = U1 * (Diagonal(S) * U2')
+#   ef = eigfact(Symmetric(U1*(Diagonal(S)*U1')), min_eigval, Inf)
+#   A11 = LowRankEigen(ef.vectors, ef.values)
 
   E1 = sub(E, :, i1)
   E2 = sub(E, :, i2)
@@ -56,6 +63,7 @@ function prepareLowRankNeighborhoodData{T<:FloatingPoint}(
   #
   zeros(K+(p-1)*L), A11, A12, A22, b1, b2, groups
 end
+
 
 function estimate_neighborhood{T<:FloatingPoint}(
     X::Matrix{T},
@@ -71,14 +79,15 @@ function estimate_neighborhood{T<:FloatingPoint}(
   L = edgeBasis.numBasis
 
   numLambda = length(λarr)
-  θ, A11, A12, A22, b1, b2, groups = prepareLowRankNeighborhoodData(X, nodeInd, nodeBasis, edgeBasis; min_eigval=min_eigval)
   #
+  θ, A11, A12, A22, b1, b2, groups = prepareLowRankNeighborhoodData(X, nodeInd, nodeBasis, edgeBasis; min_eigval=min_eigval)
   θ1 = sub(θ, 1:K)
   θ2 = sub(θ, K+1:length(θ))
   #
   θarr = Array(Float64, (numLambda, length(θ)))
   numGroups = length(groups)
   f = ProximalOPT.QuadraticFunction(A22, b2)
+  λ = zeros(numGroups)
   for indLambda=1:numLambda
     g = ProximalOPT.ProxL1L2(λarr[indLambda], groups)
     ProximalOPT.solve!(ProximalOPT.AccProxGradDescent(), θ2, f, g; options=options)
@@ -86,6 +95,87 @@ function estimate_neighborhood{T<:FloatingPoint}(
     copy!(θ1, b1)
     gemv!('N', -1., A12, θ2, -1., θ1)
     A_ldiv_B!(A11, θ1)
+    θarr[indLambda, :] = θ
+  end
+ θarr
+end
+
+
+# takes data as input and prepares everything for optimization
+# X data matrix
+# node for which we want to f
+function prepareLowRankNeighborhoodData1{T<:FloatingPoint}(
+    X::Matrix{T},
+    nodeInd::Int64,
+    nodeBasis::NodeBasis,
+    edgeBasis::EdgeBasis;
+    min_eigval::T=1e-5
+    )
+  K = nodeBasis.numBasis
+  L = edgeBasis.numBasis
+
+  D = getNeighborhoodD(X, nodeInd, nodeBasis, edgeBasis)
+  E = getNeighborhoodE(X, nodeInd, nodeBasis, edgeBasis)
+
+  n, p = size(X)
+  groups = Array(UnitRange{Int64}, p)
+  groups[1] = 1:K
+  for t=1:p-1
+    groups[t+1] = K+(t-1)*L+1:K+t*L
+  end
+
+  # A = D'D / n
+  # compute A11, A12, A22
+  # we find a low rank approximation to A
+  i1 = 1:K
+  i2 = K+1:size(D, 2)
+  svdD = svdfact(D)
+  S = svdD.S
+  m = length(S)
+  k = 0
+  @inbounds while k < m
+    k += 1
+    v = S[k].^2. / n
+    if v < min_eigval
+      k -= 1
+      break
+    end
+    S[k] = v
+  end
+  resize!(S, k)
+  U = svdD[:Vt][1:k, :]'
+  A = LowRankEigen(U, S)
+
+  b = vec( sum(E, 1) / n )
+  #
+  zeros(K+(p-1)*L), A, b, groups
+end
+
+function estimate_neighborhood1{T<:FloatingPoint}(
+    X::Matrix{T},
+    nodeInd::Int64,
+    nodeBasis::NodeBasis,
+    edgeBasis::EdgeBasis,
+    λarr::Vector{T};
+    options::ProximalOPT.ProximalOptions=ProximalOPT.ProximalOptions(),
+    min_eigval::T=1e-5
+    )
+  n, p = size(X)
+  K = nodeBasis.numBasis
+  L = edgeBasis.numBasis
+
+  numLambda = length(λarr)
+  θ, A, b, groups =  prepareLowRankNeighborhoodData1(X, nodeInd, nodeBasis, edgeBasis; min_eigval=min_eigval)
+  #
+  θarr = Array(Float64, (numLambda, length(θ)))
+  numGroups = length(groups)
+  f = ProximalOPT.QuadraticFunction(A, b)
+  λ = zeros(numGroups)
+  λ2 = sub(λ, 2:numGroups)
+  for indLambda=1:numLambda
+    fill!(λ2, λarr[indLambda])
+    g = ProximalOPT.ProxL1L2(λ, groups)
+    ProximalOPT.solve!(ProximalOPT.AccProxGradDescent(), θ, f, g; options=options)
     θarr[indLambda, :] = θ
   end
  θarr
